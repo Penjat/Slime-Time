573e2f3dc806d4f19cdfdf246de0b656ac7b169b
SpencerSymington <theSpencerSymington@gmail.com>
2018-11-07 10:19:14 -0330

    create cards, card Manager

--- Slime-Time/Assets/Scripts/Managers/MainManager.cs
+++ Slime-Time/Assets/Scripts/Managers/MainManager.cs
@@ -8,16 +8,19 @@ public class MainManager : MonoBehaviour {
 
 	private GridManager _gridManager;
 	private InputManager _inputManager;
+	private CardManager _cardManager;
 
 	void Start () {
 		Debug.Log(TAG + "Starting up.");
 
 		_gridManager = GetComponent<GridManager>();
 		_inputManager = GetComponent<InputManager>();
+		_cardManager = GetComponent<CardManager>();
 
 
 		_gridManager.SetUp(this);
 		_inputManager.SetUp(this);
+		_cardManager.SetUp(this);
 
 	}
 
