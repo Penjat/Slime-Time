93ae250a2890af8cd41a17d3d8d9af157a741b02
SpencerSymington <theSpencerSymington@gmail.com>
2018-11-07 17:28:43 -0330

    use cards and the leave the player's hand

--- Slime-Time/Assets/Scripts/Managers/MainManager.cs
+++ Slime-Time/Assets/Scripts/Managers/MainManager.cs
@@ -23,7 +23,7 @@ public class MainManager : MonoBehaviour {
 		_gridManager.SetUp(this);
 		_cardManager.SetUp(this);
 		_pieceManager.SetUp(this);
-		_inputManager.SetUp(this);
+		_inputManager.SetUp(this,_cardManager);
 
 	}
 
@@ -33,7 +33,15 @@ public class MainManager : MonoBehaviour {
 		//called when a square is pressed
 		//must figure out which manager will handel this
 
-		_pieceManager.CreatePiece(_gridManager,square);
+		//_pieceManager.CreatePiece(_gridManager,square);
+		_cardManager.SquarePressed(square);
+	}
+	public void CardPressed(Card card){
+		_cardManager.CardSelected(card);
+	}
+
+	public void EndTurn(){
+		Debug.Log(TAG + "Ending Player's Turn.");
 	}
 
 
